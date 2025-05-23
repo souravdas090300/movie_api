84aee116e8240ddea1df25b018a6ea5f78ca8fbd
souravdas090300 <souravdas090300@gmail.com>
2025-05-23 20:50:14 +0200

    Updated .gitignore
    I have added node_modules to the.gitignore file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
- 
+const express = require("express");
+const app = express();
+const http = require("http"),
+  url = require("url");
+
+http
+  .createServer((request, response) => {
+    let requestURL = url.parse(request.url, true);
+    if (requestURL.pathname == "/documentation.html") {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("Documentation on the bookclub API.\n");
+    } else {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("Welcome to my book club!\n");
+    }
+  })
+  .listen(8080);
+
+console.log("My first Node test server is running on Port 8080.");
