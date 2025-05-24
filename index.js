c5a2debe8e046cc005c3bf0317c587643c020bb6
souravdas090300 <souravdas090300@gmail.com>
2025-05-24 21:01:33 +0200

    Upadated movie_api
    I have added Express framwork, also added Morgan middleware which helps to login time in local localhost in command.

--- index.js
+++ index.js
@@ -1,29 +1,90 @@
-const express = require("express");
+const express = require("express"),
+  morgan = require("morgan");
+
 const app = express();
 
-let myLogger = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
+app.use(morgan("common"));
 
-let requestTime = (req, res, next) => {
-  req.requestTime = Date.now();
-  next();
-};
-
-app.use(myLogger);
-app.use(requestTime);
-
+// Root route
 app.get("/", (req, res) => {
-  let responseText = "Welcome to my app!";
-  responseText += "<small>Requested at: " + req.requestTime + "</small>";
-  res.send(responseText);
+  res.send(
+    "Welcome to my Movie API! Use the /movies endpoint to see my top 10 movies."
+  );
+});
+
+// GET route for the movies endpoint
+app.get("/movies", (req, res) => {
+  const top10Movies = [
+    {
+      title: "The Shawshank Redemption",
+      year: 1994,
+      director: "Frank Darabont",
+    },
+    {
+      title: "The Godfather",
+      year: 1972,
+      director: "Francis Ford Coppola",
+    },
+    {
+      title: "The Dark Knight",
+      year: 2008,
+      director: "Christopher Nolan",
+    },
+    {
+      title: "Pulp Fiction",
+      year: 1994,
+      director: "Quentin Tarantino",
+    },
+    {
+      title: "Forrest Gump",
+      year: 1994,
+      director: "Robert Zemeckis",
+    },
+    {
+      title: "Inception",
+      year: 2010,
+      director: "Christopher Nolan",
+    },
+    {
+      title: "The Matrix",
+      year: 1999,
+      director: "Lana & Lilly Wachowski",
+    },
+    {
+      title: "Parasite",
+      year: 2019,
+      director: "Bong Joon Ho",
+    },
+    {
+      title: "Interstellar",
+      year: 2014,
+      director: "Christopher Nolan",
+    },
+    {
+      title: "The Lord of the Rings: The Return of the King",
+      year: 2003,
+      director: "Peter Jackson",
+    },
+  ];
+
+  res.json(top10Movies);
+});
+
+app.get("/documentation.html", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname });
+});
+
+app.get("/index.html", (req, res) => {
+  res.sendFile("/index.html", { root: __dirname });
 });
 
 app.get("/secreturl", (req, res) => {
-  let responseText = "This is a secret url with super top-secret content.";
-  responseText += "<small>Requested at: " + req.requestTime + "</small>";
-  res.send(responseText);
+  res.send("This is a secret url with super top-secret content.");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(8080, () => {
