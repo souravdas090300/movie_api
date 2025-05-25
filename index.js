d1c11fb62317cd6f0d690039b2dd1932eb6cca9c
souravdas090300 <souravdas090300@gmail.com>
2025-05-25 15:27:04 +0200

    Updated index.js,
    .In this commit, I updated the movies array and express.static methods to ensure responses are handled properly.

--- index.js
+++ index.js
@@ -3,6 +3,59 @@ const express = require("express"),
 
 const app = express();
 
+const top10Movies = [
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    director: "Frank Darabont",
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    director: "Francis Ford Coppola",
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+  },
+  {
+    title: "Pulp Fiction",
+    year: 1994,
+    director: "Quentin Tarantino",
+  },
+  {
+    title: "Forrest Gump",
+    year: 1994,
+    director: "Robert Zemeckis",
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    director: "Christopher Nolan",
+  },
+  {
+    title: "The Matrix",
+    year: 1999,
+    director: "Lana & Lilly Wachowski",
+  },
+  {
+    title: "Parasite",
+    year: 2019,
+    director: "Bong Joon Ho",
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    director: "Christopher Nolan",
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+    director: "Peter Jackson",
+  },
+];
+
 app.use(morgan("common"));
 
 // Root route
@@ -14,69 +67,10 @@ app.get("/", (req, res) => {
 
 // GET route for the movies endpoint
 app.get("/movies", (req, res) => {
-  const top10Movies = [
-    {
-      title: "The Shawshank Redemption",
-      year: 1994,
-      director: "Frank Darabont",
-    },
-    {
-      title: "The Godfather",
-      year: 1972,
-      director: "Francis Ford Coppola",
-    },
-    {
-      title: "The Dark Knight",
-      year: 2008,
-      director: "Christopher Nolan",
-    },
-    {
-      title: "Pulp Fiction",
-      year: 1994,
-      director: "Quentin Tarantino",
-    },
-    {
-      title: "Forrest Gump",
-      year: 1994,
-      director: "Robert Zemeckis",
-    },
-    {
-      title: "Inception",
-      year: 2010,
-      director: "Christopher Nolan",
-    },
-    {
-      title: "The Matrix",
-      year: 1999,
-      director: "Lana & Lilly Wachowski",
-    },
-    {
-      title: "Parasite",
-      year: 2019,
-      director: "Bong Joon Ho",
-    },
-    {
-      title: "Interstellar",
-      year: 2014,
-      director: "Christopher Nolan",
-    },
-    {
-      title: "The Lord of the Rings: The Return of the King",
-      year: 2003,
-      director: "Peter Jackson",
-    },
-  ];
-
   res.json(top10Movies);
 });
 
-app.get("/documentation.html", (req, res) => {
-  res.sendFile("public/documentation.html", { root: __dirname });
-});
-
-app.get("/index.html", (req, res) => {
-  res.sendFile("/index.html", { root: __dirname });
-});
+app.use(express.static("public"));
 
 app.get("/secreturl", (req, res) => {
   res.send("This is a secret url with super top-secret content.");
